50c2c807d7957e074a802288e0816aa6e6fa1c61
kartikasw <kartikasariwidagdo@gmail.com>
2024-05-09 08:56:08 +0700

    fix: Bugs in sesi-empat

--- sesi-empat/main.go
+++ sesi-empat/main.go
@@ -7,17 +7,22 @@ import (
 )
 
 // Buatlah 2 concurrency dengan GOROUTINE dengan 2 data yang berbeda dengan tipe data interface keduanya.
-// Lakukan looping kedua GOROUTINE sebanyak 8 kali 
-// dalam total dimana 4 kali looping interface1 dan 4 kali looping interface2 tersebut lalu tampilkan kedua prosesnya menggunakan Println 
+// Lakukan looping kedua GOROUTINE sebanyak 8 kali
+// dalam total dimana 4 kali looping interface1 dan 4 kali looping interface2 tersebut lalu tampilkan kedua prosesnya menggunakan Println
 // Buatlah 2 kodingan dengan kondisi :
 // GOROUTINE keduanya menampilkan secara acak
-// GOROUTINE keduanya menampilkan secara rapih 
+// GOROUTINE keduanya menampilkan secara rapih
 
 func printData(data interface{}, index int) {
 	fmt.Println(data, index)
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Pilih random atau arranged.")
+		return
+	}
+
 	var wg sync.WaitGroup
 
 	switch arg := os.Args[1]; arg {
